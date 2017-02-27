eb7556974965fd76862b8a868b41593201ad9d31
Pagt0007 <gertp93@gmail.com>
2017-02-27 21:21:56 +0100

    added constraints & made gameplay table

--- database/migrations/2017_02_27_193452_create_game_table.php
+++ database/migrations/2017_02_27_193452_create_game_table.php
@@ -16,11 +16,13 @@ class CreateGameTable extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->increments('gameid');
             $table->unsignedInteger('player1id');
-            $table->unsignedInteger('player2id');
+            $table->char('player1tag',1);
+            $table->unsignedInteger('player2id')->nullable();
+            $table->char('player2tag',1)->nullable();
 
             $table->integer('gamestate');
             $table->integer('gametype'); //To check if normal tic-tac-toe or 'Extreme'
-            
+
             $table->dateTime('created_at');
             $table->dateTime('finished_at')->nullable();
 
---
+++ database/migrations/2017_02_27_200926_create_game_playtable.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNormalGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gameplay', function (Blueprint $table) {
+            $table->increments('fieldid');
+            $table->unsignedInteger('gameid');
+            $table->unsignedInteger('parentid')->nullable();
+            $table->unsignedInteger('poition1');
+            $table->unsignedInteger('poition2');
+            $table->unsignedInteger('poition3');
+            $table->unsignedInteger('poition4');
+            $table->unsignedInteger('poition5');
+            $table->unsignedInteger('poition6');
+            $table->unsignedInteger('poition7');
+            $table->unsignedInteger('poition8');
+            $table->unsignedInteger('poition9');
+            /**
+            * id's are ordered as numpad
+            * 7|8|9
+            * 4|5|6
+            * 1|2|3
+            **/
+
+            $table->foreign('parentid')
+                ->references('field_id')->on('users')
+                ->onDelete('cascade');
+                $table->foreign('gameid')
+                    ->references('gameid')->on('games')
+                    ->onDelete('cascade');
+
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
