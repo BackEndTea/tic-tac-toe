4faa09699a63f4ae3c36901b8c8bb6c690c86356
Pagt0007 <gertp93@gmail.com>
2017-02-27 20:48:30 +0100

    initial game migration

---
+++ database/migrations/2017_02_27_193452_create_game_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('gameid');
+            $table->unsignedInteger('player1id');
+            $table->unsignedInteger('player2id');
+            $table->integer('gamestate');
+            $table->integer('gametype'); //To check if normal tic-tac-toe or 'Extreme'
+
+            $table->foreign('player1id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->foreign('player2id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
