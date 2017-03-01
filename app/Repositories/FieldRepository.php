63be66a104be83e9ec204a4fed3f68efed970dd5
Pagt0007 <gertp93@gmail.com>
2017-03-01 23:20:25 +0100

    first Field repo start

---
+++ app/Repositories/FieldRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Field;
+use Illuminate\Database\Eloquent\Collection;
+
+class FieldRepository
+{
+    /**
+     * Return Game with given id from database
+     *
+     * @param $id
+     *
+     * @return mixed
+     *
+    */
+    public function getById($id)
+    {
+        return Field::findOrFail($id);
+    }
+
+    /**
+     * Returns all games from the database
+     *
+     * @return Collection|Field[]
+     *
+     */
+     public function getAll()
+     {
+         return Field::get();
+     }
+}
--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Game;
+use Illuminate\Database\Eloquent\Collection;
 
 class GameRepository
 {
