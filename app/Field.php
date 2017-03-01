1f21a705620d8414ee49adfc56b094cb1977ec53
Pagt0007 <gertp93@gmail.com>
2017-03-01 22:49:09 +0100

    created models for 2 tables

---
+++ app/Field.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Field extends Model
+{
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+   protected $table = 'field';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = "fieldid";
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+    * The attributes that aren't mass assignable.
+    *
+    * @var array
+    */
+    protected $guarded = [];
+}
