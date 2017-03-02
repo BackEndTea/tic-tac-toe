82c6fb60289aff1a3bbc356b89e063b317846d90
Pagt0007 <gertp93@gmail.com>
2017-03-02 22:40:52 +0100

    created relationships

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function games()
+    {
+        return $this->hasMany('App\Game', 'player1id')->orWhere('player2id' $this->id);
+    }
 }
