8c55fa2faf84b72daf3974bd957ddf21664177d4
Gert <gertp93@gmail.com>
2017-03-10 11:07:35 +0100

    Made helper functions and testgetbyid

--- tests/Unit/FieldRepositoryTest.php
+++ tests/Unit/FieldRepositoryTest.php
@@ -2,19 +2,78 @@
 
 namespace Tests\Unit;
 
+use App\Field;
+use App\Game;
+use App\Repositories\FieldRepository;
+use App\User;
+use App\Util\Constants;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class FieldRepositoryTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+
+    use DatabaseMigrations;
+
+    public function testGetById()
     {
-        $this->assertTrue(true);
+        $fieldRepository = new FieldRepository();
+        $field = $this->mockTestField();
+
+        $this->assertEquals($field->fieldid, $fieldRepository->getById($field->fieldid)->fieldid);
+
+
     }
+
+    private function mockTestField()
+    {
+        return $this->mockAField($this->mockAGame(
+            $this->mockaUser()->id, Constants::GAME_TYPE_NORMAL)->gameid);
+
+    }
+
+    private function mockAField($gameid)
+    {
+        return Field::create(
+            [
+                'gameid'        => $gameid,
+                'position1'     => Constants::GAME_INPUT_NONE,
+                'position2'     => Constants::GAME_INPUT_NONE,
+                'position3'     => Constants::GAME_INPUT_NONE,
+                'position4'     => Constants::GAME_INPUT_NONE,
+                'position5'     => Constants::GAME_INPUT_NONE,
+                'position6'     => Constants::GAME_INPUT_NONE,
+                'position7'     => Constants::GAME_INPUT_NONE,
+                'position8'     => Constants::GAME_INPUT_NONE,
+                'position9'     => Constants::GAME_INPUT_NONE,
+
+
+            ]
+        );
+
+    }
+
+    private function mockAGame($userID, $gameType)
+    {
+        return Game::create(
+            [
+                'player1id'     => $userID,
+                'gametype'      => $gameType,
+            ]
+        );
+
+    }
+
+    private function mockAUser()
+    {
+        return User::create(
+            [
+                'name'      => 'John Doe',
+                'email'     => 'johndoe@example.com',
+                'password'  => bcrypt('secret'),
+            ]
+        );
+    }
+
 }
